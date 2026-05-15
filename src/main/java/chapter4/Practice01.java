a2318b4ea9bfc5f21055e9c48f834de1db42e035
ImamitsuKanji <anji.imamitsu@arsnv.co.jp>
2026-05-15 17:38:37 +0900

    5月１５日　提出

---
+++ src/main/java/chapter4/Practice01.java
@@ -0,0 +1,32 @@
+package chapter4;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("chapter4/Practice01")
+public class Practice01 extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;UTF-8");
+		PrintWriter out = response.getWriter();
+
+		request.setCharacterEncoding("UTF-8");
+
+		java.time.format.DateTimeFormatter fmt = java.time.format.DateTimeFormatter.ofPattern("yyyy年MM月dd日(E)HH時㎜分ss秒");
+		String now = java.time.LocalTime.now().format(fmt);
+		out.println("""
+				<html><body>
+				<p>現在の時刻は
+				"""
+				 + now + 
+				 "です</p>");
+	}
+
+}
